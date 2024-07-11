e6eed89a92bd7f22c8002962cded868697688da7
kadosh <Noone19780506@outlook.com>
2024-07-11 20:04:17 +0300

    Static

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context: dict = {
+        'title': 'Home',
+        'content': 'Главная страница магазина -HOME'
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return render(request, 'main/about.html')
